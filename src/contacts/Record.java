5b9a7b16123d1562b5a1827f7b4c8ee15c723043
Denis Stoyanouski <vandenst@tut.by>
2023-02-15 16:56:45 +0300

    add class getters and setters and toString for Record

--- src/contacts/Record.java
+++ src/contacts/Record.java
@@ -1,9 +1,49 @@
 package contacts;
 
-import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class Record {
+class Record {
     private String name;
     private String surname;
-    private List<String> phoneNumbers;
+    private String phoneNumber = "[no number]";
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPhoneNumbers() {
+        return phoneNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        if (isValidPhoneNumber(phoneNumber)) {
+         this.phoneNumber = phoneNumber;
+        } else {
+            System.out.println("Wrong number format!");
+        }
+    }
+
+    private boolean isValidPhoneNumber(String phoneNumber) {
+        Pattern pattern = Pattern.compile("\\+?(\\([\\da-zA-Z]\\))?(\\s|-)");
+        Matcher matcher = pattern.matcher(phoneNumber);
+        return matcher.matches();
+    }
+
+    @Override
+    public String toString() {
+        return  name + " " + surname + ", " + phoneNumber;
+    }
 }
